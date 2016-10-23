88d80ff49366a42d09e0a163ab6d5d7bc4b3b58e
Kristina <Kristina>
2016-10-23 17:33:06 -0400

    help

---
+++ assets/javascript/app.js
@@ -0,0 +1,12 @@
+
+var correctAns = 0;
+var incorrectAns = 0;
+var unansweredQs = 0;
+var rightAnswer = false;
+var wrongAnswer = false;
+
+$('#presJFK').on('click', function() {
+	$('#incorrect').html(incorrectAns);
+})
+
+
