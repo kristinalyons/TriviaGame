080427320e4fe0524419c43348f1e5cbbcab3d14
Kristina <Kristina>
2016-10-23 20:16:07 -0400

    help

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,9 +1,20 @@
-
+var timer = 60;
 var correctAns = 0;
 var incorrectAns = 0;
 var unansweredQs = 0;
 var rightAnswer = false;
 var wrongAnswer = false;
+
+//this is where i want to create my code for my Time 
+//Remaining countdown from 60 seconds...
+
+setTimeout(sixtySeconds, 1000 * 5);
+
+$('#start').on('click', function() {
+	timer--;
+	$('#secondsRemaining').html(timer);
+})
+
 
 $('#presJFK').on('click', function() {
 	$('#incorrect').html(incorrectAns);
