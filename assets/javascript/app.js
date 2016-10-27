99d778b7e181e72e1af89c8f3c5465cab375be75
Kristina <Kristina>
2016-10-27 18:12:55 -0400

    homework

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -3,32 +3,58 @@
       $("#start").click(function() {
         $("#page1").hide();
         $("#page2").show();
+        timer.start()
       });
 
-        $("#page2").hide();
-        $("#page3").hide();
+      $("#page2").hide();
+      $("#page3").hide();
 
         //create 'done' button for when the user is done with the quiz
         //on click 'done' buutton:
         //.hide() page2 and .show() page3
         $("#done").click(function() {
-          $("#page2").hide();
-          $("#page3").show();
+          if (question1 != true){
+            alert("Please answer all of the questions!");
+          } 
+          else if (question2 != true){
+            alert("Please answer all of the questions!");
+          } 
+          else if (question3 != true) {
+            alert("Please answer all of the questions!");
+          }
+
+          else if (question4 != true) {
+            alert("Please answer all of the questions!");
+          }
+          else if (question5 != true) {
+            alert("Please answer all of the questions!");
+          }
+          else if (question6 != true) {
+            alert("Please answer all of the questions!");
+          }
+          else {
+            $("#page2").hide();
+            $("#page3").show();
+          }
         })
 
-      var correctAns = 0;
-      var incorrectAns = 0;
-      var unansweredQs = 0;
-      var rightAnswer = false;
-      var wrongAnswer = false;
+        var correctAns = 0;
+        var incorrectAns = 0;
+        var unansweredQs = 0;
+        var question1 = false;
+        var question2 = false;
+        var question3 = false;
+        var question4 = false;
+        var question5 = false;
+        var question6 = false;
 
 
 
-      var timer = {
-        time:0,
-        counter: null,
+        var timer = {
+          time:60,
+          counter: null,
 
-        start: function(){
+          start: function(){
         // DONE: Use setInterval to start the count here.
         timer.counter = setInterval(function(){
           timer.count();
@@ -37,7 +63,7 @@
 
       count: function(){
 
-        stopwatch.time--;
+        timer.time--;
 
         var converted = timer.timeConverter(timer.time);
         console.log(converted);
@@ -64,22 +90,29 @@
 $('#presJFK').on('click', function() {
   incorrectAns++;
   $('#incorrect').html(incorrectAns);
-
+  question1 = true
+  console.log("test")
 })
 
 $('#presGW').on('click', function() {
   incorrectAns++;
   $('#incorrect').html(incorrectAns);
+  question1 = true
+  console.log("test")
 })
 
 $('#presTR').on('click', function() {
   incorrectAns++;
   $('#incorrect').html(incorrectAns);
+  question1 = true
+  console.log("test")
 })
 
 $('#presAL').on('click', function() {
   correctAns++;
   $('#correct').html(correctAns);
+  question1 = true
+  console.log("test")
 });
 
 
@@ -87,44 +120,55 @@ $('#presAL').on('click', function() {
 $('#monWard').on('click', function() {
   incorrectAns++;
   $('#incorrect').html(incorrectAns);
-
+  question2 = true
+  console.log("test")
 })
 
 $('#macys').on('click', function() {
   incorrectAns++;
   $('#incorrect').html(incorrectAns);
+  question2 = true
+  console.log("test")
 })
 
 $('#gimbels').on('click', function() {
   correctAns++;
   $('#correct').html(correctAns);
+  question2 = true
+  console.log("test")
 })
 
 $('#jcPenney').on('click', function() {
   incorrectAns++;
   $('#incorrect').html(incorrectAns);
+  question2 = true
+  console.log("test")
 });
 
 //question #3
 $('#1933').on('click', function() {
   incorrectAns++;
   $('#incorrect').html(incorrectAns);
-
+  question3 = true
+  console.log("test")
 })
 
 $('#1928').on('click', function() {
   incorrectAns++;
   $('#incorrect').html(incorrectAns);
+  question3 = true
 })
 
 $('#1924').on('click', function() {
   correctAns++;
-  $('#incorrect').html(correctAns);
+  $('#correct').html(correctAns);
+  question3 = true
 })
 
 $('#1941').on('click', function() {
   incorrectAns++;
   $('#incorrect').html(incorrectAns);
+  question3 = true
 });
 
 
@@ -132,22 +176,27 @@ $('#1941').on('click', function() {
 $('#massasoit').on('click', function() {
   correctAns++;
   $('#correct').html(correctAns);
+  question4 = true
+  console.log("test")
 
 })
 
 $('#pemaquid').on('click', function() {
   incorrectAns++;
   $('#incorrect').html(incorrectAns);
+  question4 = true
 })
 
 $('#samoset').on('click', function() {
   incorrectAns++;
   $('#incorrect').html(incorrectAns);
+  question4 = true
 })
 
 $('#squanto').on('click', function() {
   incorrectAns++;
   $('#incorrect').html(incorrectAns);
+  question4 = true
 });
 
 
@@ -155,22 +204,27 @@ $('#squanto').on('click', function() {
 $('#nina').on('click', function() {
   incorrectAns++;
   $('#incorrect').html(incorrectAns);
+  question5 = true
+  console.log("test")
 
 })
 
 $('#pinta').on('click', function() {
   incorrectAns++;
   $('#incorrect').html(incorrectAns);
+  question5 = true
 })
 
 $('#mayflower').on('click', function() {
   correctAns++;
   $('#correct').html(correctAns);
+  question5 = true
 })
 
 $('#carnival').on('click', function() {
   incorrectAns++;
   $('#incorrect').html(incorrectAns);
+  question5 = true
 });
 
 
@@ -178,22 +232,26 @@ $('#carnival').on('click', function() {
 $('#eightMil').on('click', function() {
   incorrectAns++;
   $('#incorrect').html(incorrectAns);
+  question6 = true
 
 })
 
 $('#fifteenMil').on('click', function() {
   incorrectAns++;
   $('#incorrect').html(incorrectAns);
+  question6 = true
 })
 
 $('#thirtyThreeMil').on('click', function() {
   incorrectAns++;
   $('#incorrect').html(incorrectAns);
+  question6 = true
 })
 
 $('#fortySixMil').on('click', function() {
   correctAns++;
   $('#correct').html(correctAns);
+  question6 = true
 })
 
 });
